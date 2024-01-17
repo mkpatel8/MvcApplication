64e775b877f5cc1abccdb78f4c7b528513ad7007
Mehul Patel <mehulkpatel2@gmail.com>
2024-01-16 22:49:16 -0400

    Add project files.

---
+++ MvcApplication/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using MvcApplication.ViewModels;
+
+namespace MvcApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login(string returnUrl)
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties
+                {
+                    RedirectUri = returnUrl ?? Url.Action("Index", "Home")
+                },
+                "Auth0");
+            return new HttpUnauthorizedResult();
+        }
+
+        [Authorize]
+        public void Logout()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+            HttpContext.GetOwinContext().Authentication.SignOut("Auth0");
+        }
+
+        [Authorize]
+        public ActionResult UserProfile()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+
+            return View(new UserProfileViewModel()
+            {
+                Name = claimsIdentity?.FindFirst(c => c.Type == claimsIdentity.NameClaimType)?.Value,
+                EmailAddress = claimsIdentity?.FindFirst(c => c.Type == ClaimTypes.Email)?.Value,
+                ProfileImage = claimsIdentity?.FindFirst(c => c.Type == "picture")?.Value
+            });
+        }
+
+        [Authorize]
+        public ActionResult Claims()
+        {
+            return View();
+        }
+    }
+}
